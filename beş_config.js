9f34f2c2ab1bb0e2f41bdebf25ecef65d5493215
Vultrush <117504260+Vultrush@users.noreply.github.com>
2023-11-16 20:35:15 +0300

    ilk gönderme

---
+++ beş_config.js
@@ -0,0 +1,41 @@
+module.exports = {
+
+token:"MTE3NDYyNzY5NTk5NzUwNTU0Nw.GPjCJj.ZMbvbc5aczvuR5-m3oUdhfXJJlywGm_7nyKr_U", //Bot Tokeniniz => Bot Oluşturduktan Sonra Intentsleri Açmayı Unutma!
+guildID:"539044376584650754", //Kullanıcağın Sunucunun ID'si
+botDurum:"NİGHT Was Here", //Botun Durumu
+footer:"Vultrush", //Embedların Footer Yazısı
+botOwners:["495626751704039425"], //Bot Sahibinin ID'si
+prefix:".", //Botun Prefix'i 
+manemoji:"♂️", //Erkek Emojisi
+womanemoji:"♀️", //Kadın Emojisi
+tagSymbol:"5", //Sunucunun Taglı İsim Sembolu => Eğer Tagınız Yoksa Boş Bırakın Veya "5" Kalsın
+normalSymbol:"•", //Normal Kayıt Olucak Kullanıcıların Başına Gelicek Sembol
+symbolBeş:"|", //Kayıt Esnasında İsim Ve Yaş'ı Ayıran Çizgi
+minageAge:15, //En Düşük Kayıt Edilebilir Yaş
+welcomeResimURL:"https://cdn.discordapp.com/attachments/950167988127006821/1088263193832472656/beeautiful-sunset-illustration-1212023.png", //Resimli Hoşgeldin Tasarımının Arkaplan'ı => URL Şeklinde Giriniz / gif veya webp Uzantısı Kabul Etmez  
+welcomeResimRenk:"#861765", //Resimli Hoşgeldin Tasarımının Çerçeve Rengi => HexColor Şeklinde Giriniz
+kayitsizHesapIsim:"İsim | Yaş", //Kayıtsızların Düzeltileceği İsim
+supheliHesapIsim:"Şüpheli", //Şüpheli Hesapların Düzeltileceği İsim
+
+
+//Aşşağıdakiler Hoşgeldinde Gözüken Sayı Emojileridir Kendiniz Doldurmak İsterseniz Doldurursunuz
+
+//Eğer Buraya Kadar Yaptıysan .setup Komudunu Kullanarak Botu Sunucuna Kurmaya Başla.
+
+sayılarEmoji:{
+sıfır:"0",
+bir:"1",
+iki:"2",
+üç:"3",
+dört:"4",
+beş:"5",
+altı:"6",
+yedi:"7",
+sekiz:"8",
+dokuz:"9"
+}
+
+
+
+
+}
